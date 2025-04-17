d092ad6c0a2a35f43d35abbe748fbd2e63ca1468
Patriotic20 <pterest160@gmail.com>
2025-04-17 16:38:12 +0500

    add to main api

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from .auth import auth_router
 from .user import user_router
-
+from .admin import admin_router
 
 api_router = APIRouter()
 
 
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
+api_router.include_router(admin_router)
