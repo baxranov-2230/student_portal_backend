940d1b5253fa1c5c402e7796820b92752506848e
Ahror <axrorwillberich2230@gmail.com>
2025-06-23 12:49:23 +0500

    alembic update

--- src/models/application.py
+++ src/models/application.py
@@ -9,7 +9,7 @@ class Application(Base):
     user_id = Column(ForeignKey("users.id"))
     full_name = Column(String , nullable=True)
     student_id_number = Column(String, nullable=True)
-    image_path = Column(String, nullable=True) 
+    image_path = Column(String, nullable=True)
     group = Column(String, nullable=True)
     faculty = Column(String, nullable=True) 
     gpa = Column(Float , nullable=True)
